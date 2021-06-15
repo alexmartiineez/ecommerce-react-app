9cd048fe2ceebe08bf0546e0a8151d11bf554700
Alexander Martinez <aemartinez1998@gmail.com>
2021-06-15 23:54:38 +0200

    ..

--- src/screens/PlaceOrder/PlaceOrder.jsx
+++ src/screens/PlaceOrder/PlaceOrder.jsx
@@ -42,75 +42,77 @@ const PlaceOrder = () => {
     console.log(order)
 
     return (
-        <div className="container mt-3 container-placeholder">
+        <div className='container mt-3 container-placeorder'>
+            <div className='row d-flex'>
 
-            <div className="row d-flex">
-
-                <div className="col-md-8">
-
-                    <div className="card p-3 mt-3 bg-light">
-                        <h5 className="card-title text-capitalize mb-3">Datos de envio</h5>
+                <div className='col-md-8'>
+                    <div className='card p-3 mt-3 bg-ligth'>
+                        <h5 className='card-title text-capitalize mb-3'>
+                            Datos de envio
+                        </h5>
+                        <div>
+                            <p className='mb-1 fw-bold'>
+                                Nombre: <span className='fw-normal'>nombre de orden</span>
+                            </p>
+                            <p>
+                                Domicilio: <span className='fw-normal'>argentina</span>
+                            </p>
+                        </div>
                     </div>
-                    <div className="">
-                        <p className="mb-1 fw-bold">
-                            Nombre: 
-                                <span className="fw-normal">nombre de orden</span>
+
+                    <div className='card p-3 mt-3 bg-light'>
+                        <h5 className='card-title text-capitalize mb-3'>
+                            Medio de pago
+                        </h5>
+                        <p className='mb-1 fw-bold'>
+                            Metodo: 
+                            <span className='fw-bold text-uppercase'>
+                                Mercado Pago
+                            </span>
                         </p>
-                        <p className="mb-1 fw-bold">
-                            Domicilio: 
-                                <span className="fw-normal">argentina</span>
+                        <p className='mb-1 fw-bold'>
+                            Estado:
+                            <span className='fw-bold text-uppercase'>
+                                Not paid
+                            </span>
                         </p>
                     </div>
+                    
+                    <div className='card p-3 mt-3 bg-light'>
+                        <h5 className='card-title text-capitalize mb-3'>
+                            Detalles de la orden
+                        </h5>
+                        <div>
+                            {
+                                //ACA IR EL CARRITO
+                            }
+                        </div>
+                    </div>
+
                 </div>
 
-                <div className="card p-3 mt-3 bg-light">
-                    <h5 className="card-title text-capitalize mb-3">Metodo de pago</h5>
-                    <p className="mb-1 fw-bold">
-                        Metodo:
-                            <span className="fw-bold text-uppercase">Mercado Pago</span>
-                    </p>
-                    <p className="mb-1 fw-bold">
-                        Estado:
-                            <span className="fw-bold text-uppercase">Not Paid</span>
-                    </p>
-                </div>
 
-                <div className="card p-3 mt-3 bg-light">
-                    <h5 className="card-title text-uppercase mb-3">Detalles de la compra</h5>
-                    <div>
-                        {
-                            //ACA VA EL CARRITO
-                        }
+                <div className='col-md-4'>
+                    <div className='card mt-3 bg-light p-3'>
+                        <div className='d-flex justify-content-between'>
+                            <span className='fw-bold'>Subtotal</span>
+                            <span>$ 40000</span>
+                        </div>
+                        <div className='d-flex justify-content-between'>
+                            <span className='fw-bold'>Descuento</span>
+                            <span>$ 3000</span>
+                        </div>
+                        <hr/>
+                        <div className='d-flex justify-content-between'>
+                            <span className='fw-bold'>Total</span>
+                            <span>$ 37000</span>
+                        </div>
                     </div>
                 </div>
 
             </div>
+
             
-            <div className="col-md-4">
-
-                <div className="card mt-3 bg-light p-3">
-
-                    <div className="d-flex justify-content-between">
-                        <span className="fw-bold">Subtotal</span>
-                        <span>$ 40.000</span>
-                    </div>
-
-                    <div className="d-flex justify-content-between">
-                        <span className="fw-bold">Descuento</span>
-                        <span>$ 3.000</span>
-                    </div>
-
-                    <hr />
-
-                    <div className="d-flex justify-content-between">
-                        <span className="fw-bold">Total</span>
-                        <span>$ 37.000</span>
-                    </div>
-
-                </div>
-                
-            </div>
-
         </div>
     )
 }
